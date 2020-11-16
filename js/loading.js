64a4224e70825dbb16c3e3fe7182daa8e24b24be
“jurii-isaka” <neyzs2az@gmail.com>
2020-11-16 19:10:01 +0900

    fix0

---
+++ js/loading.js
@@ -0,0 +1,13 @@
+
+//ページの読み込みが完了したらアニメーションを非表示
+// $(window).on('load',function(){
+//  $('.loader-wrap').fadeOut();
+//  $('#container').fadeIn();
+// });
+
+//ページの読み込みが完了して、3秒後にアニメーションを非表示にする
+setTimeout(() =>{
+    $('.loader').fadeOut();
+    $('#container').fadeIn();
+    $('header').fadeIn();
+},1100);
